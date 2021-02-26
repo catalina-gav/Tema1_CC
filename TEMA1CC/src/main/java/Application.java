27b402861f62122770f2071859c4b09f674d580d
catalina-gav <61465343+catalina-gav@users.noreply.github.com>
2021-02-26 17:55:59 +0200

    server

--- TEMA1CC/src/main/java/Application.java
+++ TEMA1CC/src/main/java/Application.java
@@ -19,9 +19,9 @@ class Application {
 
     public static HashMap<String,String> ApiCall(String urlString) throws IOException {
         URL url = new URL(urlString);
+        long start = System.currentTimeMillis();
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
-        long start = System.currentTimeMillis();
         int status = con.getResponseCode();
         long millis = System.currentTimeMillis() - start;
 
@@ -41,10 +41,29 @@ class Application {
         response.put("apiResp",respText);
         response.put("apiStatus",respCode);
         response.put("latency",latency);
-        System.out.println(latency);
         return  response;
 
     }
+
+    public static  String getLatestStatus(String route){
+        String latestStatus="unknown";
+        try {
+            File myObj = new File("D:\\activity.log");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if(data.contains(route+ " is")){
+                    latestStatus=data;
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return latestStatus;
+    }
+
     public static String getNOfParallelRuns(){
         int nOfRuns=0;
         try {
@@ -88,6 +107,7 @@ class Application {
         }
         return Integer.toString(nOfCalls);
     }
+
     public static String getAverageLatency(String api){
         String latencyType="";
         if(api.equals("random")){
@@ -122,19 +142,22 @@ class Application {
         String average=Integer.toString(averageLatency);
         return  average;
     }
+
     public static String doCycle(Logger logger,String route) throws IOException {
-        System.out.println(route);
+
         FileInputStream ip= new FileInputStream("D:\\TemeCC\\TEMA1CC\\src\\main\\java\\config.properties");
         Properties prop=new Properties();
         prop.load(ip);
+        //keys
         String yt_api_key=prop.getProperty("yt_key");
-        System.out.println("YT KEY: " + yt_api_key);
         String geo_api_key=prop.getProperty("geo_key");
+        //calls
         String locationCall="http://api.positionstack.com/v1/forward?access_key="+geo_api_key+"&&query=Suceava";
         String randomNumberCall="https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new";
-        String latitude;
-        String longitude;
-        String maxResult;
+
+        String latitude="";
+        String longitude="";
+        String maxResult="";
         String resp="{\"error\":\"Something went wrong\"}";
         logger.info("request to server on " + route);
         logger.info("Call to Location Api");
@@ -144,44 +167,49 @@ class Application {
         String locationLatency=locationMap.get("latency");
         logger.info("Response code location API : " + locationStatus);
         logger.info("latency_location: " + locationLatency + "ms");
-        System.out.println(locationStatus);
+
         try {
 
             JSONObject jsonObject=new JSONObject(locationResp);
             JSONArray jsonArray=new JSONArray(jsonObject.getString("data"));
             latitude=jsonArray.getJSONObject(0).getString("latitude");
             longitude=jsonArray.getJSONObject(0).getString("longitude");
-            logger.info("Call to Random Api");
-            HashMap<String,String> randomMap=ApiCall(randomNumberCall);
-            String randomResp=randomMap.get("apiResp");
-            String randomStatus=randomMap.get("apiStatus");
-            String randomLatency=randomMap.get("latency");
-            logger.info("Response code random API : " + randomStatus);
-            logger.info("latency_random: " + randomLatency + "ms");
-            maxResult=randomResp;
-            System.out.println(latitude + " " + longitude);
-            String youtubeCall="https://youtube.googleapis.com/youtube/v3/search?" +
-                    "part=snippet&location="+latitude+","+longitude+"&locationRadius=50km&maxResults="+maxResult+"&order=rating&type=video&fields=items.id.videoId%2Citems.snippet.title&key=" + yt_api_key;
-            logger.info("Call to Youtube Api");
-            HashMap<String,String> youtubeMap=ApiCall(youtubeCall);
-            String youtubeResp=youtubeMap.get("apiResp");
-            String youtubeStatus=youtubeMap.get("apiStatus");
-            String youtubeLatency=youtubeMap.get("latency");
-            logger.info("Response code youtube API : " + youtubeStatus);
-            logger.info("latency_youtube: " + youtubeLatency + "ms");
-            System.out.println(randomResp);
-            resp = youtubeResp;
-            System.out.println(resp);
-            if(youtubeStatus.equals("200") && locationStatus.equals("200") && randomStatus.equals("200")){
-                logger.info(route+ " is alive.");
-            }else{
-                logger.info(route+" is not alive.");
-            }
+
         } catch (JSONException e) {
-            e.printStackTrace();
+            System.out.println("Location Api didnt return data...retrying...");
+        }
+        logger.info("Call to Random Api");
+        HashMap<String,String> randomMap=ApiCall(randomNumberCall);
+        String randomResp=randomMap.get("apiResp");
+        String randomStatus=randomMap.get("apiStatus");
+        String randomLatency=randomMap.get("latency");
+        logger.info("Response code random API : " + randomStatus);
+        logger.info("latency_random: " + randomLatency + "ms");
+        maxResult=randomResp;
+        //yt call
+        String youtubeCall="https://youtube.googleapis.com/youtube/v3/search?" +
+                "part=snippet&location="+latitude+","+longitude+"&locationRadius=50km&maxResults="+maxResult+"&order=rating&type=video&fields=items.id.videoId%2Citems.snippet.title&key=" + yt_api_key;
+        logger.info("Call to Youtube Api");
+        HashMap<String,String> youtubeMap=ApiCall(youtubeCall);
+        String youtubeResp=youtubeMap.get("apiResp");
+        String youtubeStatus=youtubeMap.get("apiStatus");
+        String youtubeLatency=youtubeMap.get("latency");
+        logger.info("Response code youtube API : " + youtubeStatus);
+        logger.info("latency_youtube: " + youtubeLatency + "ms");
+
+        resp = youtubeResp;
+
+        if(resp.equals("")){
+            resp="{\"error\":\"Something went wrong\"}";
+        }
+        if(youtubeStatus.equals("200") && locationStatus.equals("200") && randomStatus.equals("200")){
+            logger.info(route+ " is alive.");
+        }else{
+            logger.info(route+" is not alive.");
         }
         return resp;
     }
+
     static class Task implements Callable<Integer> {
         private static Random rand = new Random();
         private final int no;
@@ -215,8 +243,7 @@ class Application {
     }
 
     public static void main(String[] args) throws IOException {
-        boolean append = true;
-        FileHandler handler = new FileHandler("D:\\activity.log", append);
+        FileHandler handler = new FileHandler("D:\\activity.log", true);
         logger.addHandler(handler);
         SimpleFormatter formatter = new SimpleFormatter();
         handler.setFormatter(formatter);
@@ -226,8 +253,6 @@ class Application {
         int serverPort = 8000;
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
         server.createContext("/api/cycle", (exchange -> {
-
-
             if ("GET".equals(exchange.getRequestMethod())) {
                String resp=doCycle(logger,"/api/cycle");
                 exchange.sendResponseHeaders(200, resp.getBytes().length);
@@ -240,7 +265,6 @@ class Application {
                 exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
                 logger.info("server response code = 405");
             }
-
             exchange.close();
 
         }));
@@ -248,16 +272,19 @@ class Application {
             logger.info("call to /api/parallel");
             ExecutorService executor = Executors.newFixedThreadPool(5);
             try {
-                logger.info("parallel run");
+                logger.info("start of parallel run");
+                logger.info("first batch");
                 processBatch(executor, 1,logger);
+                logger.info("second batch");
                 processBatch(executor, 2,logger);
+                logger.info("third batch");
                 processBatch(executor, 3,logger);
                 executor.shutdown();
                 logger.info("end of parallel run");
 
-                exchange.sendResponseHeaders(200,"Ok".getBytes().length);
+                exchange.sendResponseHeaders(200,"{\"message\":\"Ok\"}".getBytes().length);
                 OutputStream output = exchange.getResponseBody();
-                output.write("OK".getBytes());
+                output.write("{\"message\":\"Ok\"}".getBytes());
                 output.flush();
                 exchange.close();
 
@@ -277,9 +304,42 @@ class Application {
             String nOfYtCalls=getNumberOfApiCalls("youtube");
             String nOfLocCalls=getNumberOfApiCalls("location");
             String nOfParallelRuns=getNOfParallelRuns();
-            exchange.sendResponseHeaders(200,"OK".getBytes().length);
+            String parallelStatus=getLatestStatus("/api/parallel");
+            String cycleStatus=getLatestStatus("/api/cycle");
+            if(!parallelStatus.contains("unknown") && !parallelStatus.contains("not alive")){
+                parallelStatus="alive";
+            }else{
+                if(!parallelStatus.contains("unknown")){
+                    parallelStatus="not alive";
+                }
+            }
+            if(!cycleStatus.contains("unknown") && !cycleStatus.contains("not alive")){
+                cycleStatus="alive";
+            }else{
+                if(!cycleStatus.contains("unknown")){
+                    cycleStatus="not alive";
+                }
+            }
+            JSONObject jsonObject=new JSONObject();
+            try {
+                jsonObject.put("yt_lat",averageYoutube);
+                jsonObject.put("rand_lat",averageRandom);
+                jsonObject.put("loc_lat",averageLocation);
+                jsonObject.put("rand_calls",nOfRandomCalls);
+                jsonObject.put("loc_calls",nOfLocCalls);
+                jsonObject.put("yt_calls",nOfYtCalls);
+                jsonObject.put("parallel_count",nOfParallelRuns);
+                jsonObject.put("statusCycle",cycleStatus);
+                jsonObject.put("statusParallel",parallelStatus);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            exchange.sendResponseHeaders(200,jsonObject.toString().getBytes().length);
             OutputStream output = exchange.getResponseBody();
-            output.write("Ok".getBytes());
+            output.write(jsonObject.toString().getBytes());
             output.flush();
             exchange.close();
 
